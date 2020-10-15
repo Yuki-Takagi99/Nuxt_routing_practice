3e602207776c2f39db920699857ab12dd5e3829a
yuki takagi <yuki99takagi@gmail.com>
2020-10-15 19:05:42 +0900

    Add 動的なルーティング

---
+++ pages/users/_id.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <p>id: {{ this.$route.params.id }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "_id",
+  data: function(){
+    return {
+      message: '/users/_id.vueを表示中'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
